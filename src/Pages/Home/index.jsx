5282d0c03639c91eec727f3690c7a260d8676512
xMarioOP <marioalejandro424@outlook.com>
2024-10-09 16:26:28 -0500

    Setup API Fetching for Product List

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,32 @@
+import { useState, useEffect } from "react";
 import { Card } from "../../Components/Card";
 import { Layout } from "../../Components/Layout";
 
 const Home = () => {
+    const [items, setItems] = useState(null)
+
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+            .then(response => response.json()
+                .then(data => setItems(data))
+            )
+    }, [])
+
     return (
         <Layout className="bg-red-100">
             Home
-            <Card/>
+            <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+                {
+                    items?.map(item => (
+                        <Card
+                            key={item.id}
+                            data={item}
+                        />
+                    ))
+                }
+            </div>
         </Layout>
     );
 };
 
-export {Home};
+export { Home };

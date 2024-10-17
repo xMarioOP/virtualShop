b297f5437f500e2468cd7b0139ddf2b5344a66cb
xMarioOP <marioalejandro424@outlook.com>
2024-10-17 14:58:53 -0500

    ProductDetail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -9,7 +9,7 @@ const ProductDetail = () => {
 
     return (
         <aside
-            className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}
+            className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} overflow-auto product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}
         >
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Detail</h2>
